c626488b3979ea4cfef86263ccc24718b1e52c66
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-09-18 22:27:37 +0700

    feat: run on waylan

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -4,6 +4,22 @@ import { overlayIPC } from "./ipc/overlay";
 import { yomitanIPC } from "./ipc/yomitan";
 import { mainWindow } from "./window/main";
 
+// NOTE: Workaround for https://github.com/electron/electron/issues/41614
+app.on("web-contents-created", (event, contents) => {
+  contents.on("devtools-opened", () =>
+    contents.devToolsWebContents?.executeJavaScript(`
+        (() => {
+            const origErr = console.error;
+            console.error = function (...args) {
+                const s = String(args[0] ?? "");
+                if (s.includes("Autofill.enable") || s.includes("Autofill.setAddresses")) return;
+                return origErr.apply(console, args);
+            };
+        })()
+    `),
+  );
+});
+
 app.whenReady().then(() => {
   logIPC.register();
   overlayIPC.register();
