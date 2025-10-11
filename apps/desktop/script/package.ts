122b10a02848af1c9a41ec7cd8c551d156ee3ce2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-10-12 00:04:32 +0700

    build: update script

--- apps/desktop/script/package.ts
+++ apps/desktop/script/package.ts
@@ -1,11 +1,11 @@
 import { rm } from "node:fs/promises";
 import packager from "@electron/packager";
 
-await rm("./release/seruni-win32-x64", { recursive: true });
+await rm("./.release/seruni-win32-x64", { recursive: true, force: true });
 
 await packager({
   dir: "./dist",
-  out: "./release",
+  out: "./.release",
   platform: ["win32"],
   icon: "./icons/seruni.ico",
 });
