d669269b6d4c628507ff92d9cc57af1c5009d730
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-10-19 01:34:27 +0700

    feat: crop on zoom

--- packages/renderer/src/routes/-components/HistoryTab/EditButton.tsx
+++ packages/renderer/src/routes/-components/HistoryTab/EditButton.tsx
@@ -370,6 +370,139 @@ function ImageWithFallback(props: {
   );
 }
 
+function CropperImage(props: { src: string; editing: boolean }) {
+  const [selectionData, setSelectionData] = createSignal<SelectionData>({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+  const [naturalSize, setNaturalSize] = createSignal<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
+  const [isPreselect, setIsPreselect] = createSignal(true);
+  const image = new Image();
+  image.alt = "Picture";
+  image.src = props.src;
+  let cropperContainer: HTMLDivElement | undefined;
+  let cropper: Cropper | undefined;
+
+  function inSelection(selection: SelectionData, maxSelection: SelectionData) {
+    return (
+      selection.x >= maxSelection.x &&
+      selection.y >= maxSelection.y &&
+      selection.x + selection.width <= maxSelection.x + maxSelection.width &&
+      selection.y + selection.height <= maxSelection.y + maxSelection.height
+    );
+  }
+
+  const abortController = new AbortController();
+
+  createEffect(() => {
+    image.src = props.src;
+    cropper = new Cropper(image, {
+      container: cropperContainer,
+    });
+
+    const listener1 = (e: Event) => {
+      const image = e.target as HTMLImageElement;
+      setNaturalSize({
+        width: image.naturalWidth,
+        height: image.naturalHeight,
+      });
+    };
+    image.addEventListener("load", listener1);
+
+    const centerImage = () => {
+      cropper?.getCropperImage()?.$center("contain");
+      if (props.editing && isPreselect()) {
+        const cropperSelection = cropper?.getCropperSelection();
+        const w = image.naturalWidth;
+        const h = image.naturalHeight;
+
+        // target crop size (50%)
+        const cropWidth = w * 0.5;
+        const cropHeight = h * 0.5;
+
+        // centered position
+        const cropX = (w - cropWidth) / 2;
+        const cropY = (h - cropHeight) / 2;
+
+        cropperSelection?.$change(cropX, cropY, cropWidth, cropHeight);
+        setIsPreselect(false);
+      }
+    };
+    const observer = new ResizeObserver(centerImage);
+    const cropperImage = cropper.getCropperImage();
+    if (cropperImage) observer.observe(cropperImage);
+    window.addEventListener("resize", centerImage);
+
+    const listener2 = (e: Event) => {
+      cropper?.getCropperImage()?.$center("contain");
+      const parsed = zSelectionData.safeParse(
+        (e as Event & { detail: unknown }).detail,
+      );
+      if (parsed.success) {
+        const cropperImageRect = cropper
+          ?.getCropperImage()
+          ?.getBoundingClientRect();
+        const maxSelection: SelectionData = {
+          x: 0,
+          y: 0,
+          width: cropperImageRect?.width ?? 0,
+          height: cropperImageRect?.height ?? 0,
+        };
+        if (!inSelection(parsed.data, maxSelection)) {
+          e.preventDefault();
+        } else {
+          setSelectionData(parsed.data);
+        }
+      }
+    };
+    cropper.getCropperSelection()?.addEventListener("change", listener2);
+
+    abortController.signal.addEventListener("abort", () => {
+      image.removeEventListener("load", listener1);
+      cropper?.getCropperSelection()?.removeEventListener("change", listener2);
+      observer.disconnect();
+      window.removeEventListener("resize", centerImage);
+    });
+
+    onCleanup(() => {
+      cropper?.getCropperCanvas()?.remove();
+      abortController.abort();
+    });
+  });
+
+  createEffect(() => {
+    console.log("DEBUG[760]: selectionData=", selectionData());
+  });
+  return (
+    <div
+      ref={cropperContainer}
+      class={css({
+        maxW: "full",
+        rounded: "md",
+        shadow: "md",
+        "& > cropper-canvas": {
+          height: "full",
+          width: "full",
+        },
+        "& > cropper-canvas > cropper-image": {
+          height: "full",
+          width: "full",
+        },
+      })}
+      style={{
+        width: `${naturalSize().width}px`,
+        "aspect-ratio": `${naturalSize().width} / ${naturalSize().height}`,
+        height: "auto",
+      }}
+    ></div>
+  );
+}
+
 export function PictureWithCropper(props: {
   src: string;
   trigger: (props: () => ParentProps) => JSX.Element;
@@ -520,6 +653,8 @@ export function PictureWithZoom(props: {
   src: string;
   trigger: (props: () => ParentProps) => JSX.Element;
 }) {
+  const [editing, setEditing] = createSignal(false);
+
   return (
     <Dialog.Root>
       <Dialog.Trigger
@@ -541,18 +676,56 @@ export function PictureWithZoom(props: {
           >
             <Dialog.CloseTrigger
               asChild={(closeTriggerProps) => (
-                <img
-                  {...closeTriggerProps()}
-                  class={css({
-                    w: "full",
-                    maxW: "8xl",
-                    objectFit: "contain",
-                    rounded: "md",
-                    shadow: "md",
-                  })}
-                  src={props.src}
-                  alt="PictureField"
-                />
+                <Stack>
+                  <Show when={!editing()}>
+                    <img
+                      {...closeTriggerProps()}
+                      class={css({
+                        w: "full",
+                        maxW: "8xl",
+                        objectFit: "contain",
+                        rounded: "md",
+                        shadow: "md",
+                      })}
+                      src={props.src}
+                      alt="PictureField"
+                    />
+                  </Show>
+
+                  <Box maxW="8xl" hidden={!editing()}>
+                    <CropperImage src={props.src} editing={editing()} />
+                  </Box>
+
+                  <HStack justifyContent="end" px="8">
+                    <Button
+                      {...closeTriggerProps()}
+                      opacity="0"
+                      flex="1"
+                      cursor="default"
+                    >
+                      ""
+                    </Button>
+                    <Show when={editing()}>
+                      <Button
+                        onClick={() => {
+                          setEditing(false);
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                      <Button onClick={() => {}}>Save</Button>
+                    </Show>
+                    <Show when={!editing()}>
+                      <Button
+                        onClick={() => {
+                          setEditing(true);
+                        }}
+                      >
+                        Crop
+                      </Button>
+                    </Show>
+                  </HStack>
+                </Stack>
               )}
             />
           </Dialog.Content>
