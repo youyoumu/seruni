0e8badc7c6d1f064110be561649f2c45d057f9fd
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-04 13:08:43 +0700

    build: add hero ui

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -3,7 +3,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
-import appCss from "../styles.css?url";
+import appCss from "../styles/main.css?url";
 
 interface MyRouterContext {
   queryClient: QueryClient;
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@heroui/react";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return "test";
+  return <Button>My Button</Button>;
 }
