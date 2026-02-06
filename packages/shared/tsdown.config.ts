95a9f6a5a25c0d183ba51a0d683eb3027018c98a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-06 16:46:30 +0700

    build: use d.ts

---
+++ packages/shared/tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: [
+    './src/db/index.ts',
+    './src/ws/index.ts',
+    './src/types/index.ts',
+  ],
+  format: ["es"],
+  dts: true,
+  sourcemap: true,
+  unbundle: true,
+})
