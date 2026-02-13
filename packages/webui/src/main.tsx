1a27c9f8d26814902d3d008a02b7d41731637f5a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-13 22:48:20 +0700

    refactor: oxlint rules

--- packages/webui/src/main.tsx
+++ packages/webui/src/main.tsx
@@ -31,8 +31,8 @@ declare module "@tanstack/react-router" {
   }
 }
 
-const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
+const rootElement = document.getElementById("root");
+if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
