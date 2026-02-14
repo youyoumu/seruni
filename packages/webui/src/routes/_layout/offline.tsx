649df6bcf512754283822e933b2bb8cd0820b893
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-14 16:11:04 +0700

    fix: loading page stuck

--- packages/webui/src/routes/_layout/offline.tsx
+++ packages/webui/src/routes/_layout/offline.tsx
@@ -1,4 +1,4 @@
-import { useOnline, useServices } from "#/hooks/api";
+import { useServices } from "#/hooks/api";
 import { createFileRoute, useLocation } from "@tanstack/react-router";
 import { ShellIcon } from "lucide-react";
 import { useEffect, useRef } from "react";
@@ -17,29 +17,37 @@ export const Route = createFileRoute("/_layout/offline")({
 });
 
 function RouteComponent() {
-  /* //TODO: pretty loading */
   const { redirect } = Route.useSearch();
   const navigate = Route.useNavigate();
   const { ws } = useServices();
   const location = useLocation();
   const timeoutId = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
 
+  const getRedirectPath = () => {
+    if (!redirect) return "/";
+    if (redirect === "/offline") return "/";
+    return redirect;
+  };
+  const redirectPath = getRedirectPath();
+
   useEffect(() => {
     const handler = () => {
       ws.removeEventListener("open", handler);
       if (timeoutId.current) clearTimeout(timeoutId.current);
       timeoutId.current = setTimeout(() => {
+        //TODO: search params
+        // @ts-expect-error dynamic
         navigate({
-          to: redirect,
-          search: { redirect: location.pathname },
+          to: redirectPath,
         });
       }, 1000);
     };
     ws.addEventListener("open", handler);
+
     return () => {
       ws.removeEventListener("open", handler);
     };
-  }, [navigate, ws, location, redirect]);
+  }, [navigate, ws, location, redirectPath]);
 
   return (
     <div className="flex h-screen items-center justify-center">
