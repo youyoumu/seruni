7af5e4ea693c2bff6ef92c7ea7afa9b52d2fc4d0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-14 17:50:19 +0700

    feat: font

--- packages/webui/vite.config.ts
+++ packages/webui/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 const config = defineConfig({
   resolve: {
@@ -17,6 +18,10 @@ const config = defineConfig({
     tanstackRouter({
       target: "react",
     }),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap",
+      "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
+    ]),
   ],
 });
 
