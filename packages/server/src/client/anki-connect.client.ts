9a861411880e2bf295fd1dc7148ecc1236bab5fa
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-22 14:59:29 +0700

    feat: filesToDelete

--- packages/server/src/client/anki-connect.client.ts
+++ packages/server/src/client/anki-connect.client.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs/promises";
 import path from "node:path";
 
 import type { DB } from "#/db";
@@ -14,13 +15,22 @@ import z from "zod";
 import type { OBSClient } from "./obs.client";
 import { ReconnectingAnkiConnect } from "./ReconnectingAnkiConnect";
 
+class UpdateError extends Error {
+  filesToDelete: string[];
+  constructor(message: string, filesToDelete: string[] = []) {
+    super(message);
+    this.filesToDelete = filesToDelete;
+  }
+}
+
 type ProcessQueueResult =
   | {
       picturePath: string;
       sentenceAudioPath: string;
       reuseMedia?: true;
+      filesToDelete: string[];
     }
-  | Error;
+  | UpdateError;
 
 export class AnkiConnectClient extends ReconnectingAnkiConnect {
   state: State;
@@ -61,7 +71,6 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
     });
   }
 
-  // TODO: clean savedReplayPath and audioStage1Path
   async updateNoteMedia({
     note,
     textHistoryId,
@@ -76,9 +85,9 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
     const history = await this.db.query.textHistory.findFirst({
       where: eq(textHistory.id, textHistoryId),
     });
-    if (!history) return Error("Failed to find text history");
+    if (!history) return new UpdateError("Failed to find text history");
     if (history.createdAt.getTime() < now.getTime() - REPLAY_BUFFER_DURATION) {
-      return Error("Text history already pass the replay buffer duration");
+      return new UpdateError("Text history already pass the replay buffer duration");
     }
 
     this.log.trace(
@@ -89,11 +98,15 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
       "Using history",
     );
 
+    const filesToDelete: string[] = [];
     const alreadyProcessed = this.processQueue.has(history.id);
     const { promise, resolve: resolve_ } = Promise.withResolvers<ProcessQueueResult>();
     const resolve = (result: ProcessQueueResult) => {
       resolve_(result);
       return result;
+    };
+    const error = (error: Error) => {
+      return resolve(new UpdateError(error.message, filesToDelete));
     };
     if (!alreadyProcessed) this.processQueue.set(history.id, promise);
 
@@ -101,7 +114,7 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
     if (alreadyProcessed) {
       this.log.info("Trying to reuse media files");
       const result = await this.processQueue.get(history.id);
-      if (!result) return Error("Can't find process queue with id: " + history.id);
+      if (!result) return new UpdateError("Can't find process queue with id: " + history.id);
       if (result instanceof Error) return result;
       if (result) {
         this.log.trace({ ...result }, "Reusing media files");
@@ -110,19 +123,21 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
           picturePath: result.picturePath,
           sentenceAudioPath: result.sentenceAudioPath,
         });
-        if (updateResult instanceof Error) return updateResult;
+        if (updateResult instanceof Error) return new UpdateError(updateResult.message);
         return { ...result, reuseMedia: true };
       }
     }
 
     // save replay buffer
     const savedReplayPath = await this.obsClient.saveReplayBuffer();
-    if (savedReplayPath instanceof Error) return resolve(savedReplayPath);
+    if (savedReplayPath instanceof Error) return error(savedReplayPath);
+    filesToDelete.push(savedReplayPath);
 
     // calculate offset
     const fileEnd = new Date();
     const duration = await this.ffmpeg.getFileDuration(savedReplayPath);
-    if (duration instanceof Error) return resolve(duration);
+    if (duration instanceof Error) return error(duration);
+
     const fileStart = new Date(fileEnd.getTime() - duration);
     const offset = Math.max(0, Math.floor(history.createdAt.getTime() - fileStart.getTime()));
 
@@ -132,17 +147,18 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
       seek: offset,
       format: "wav",
     });
-    if (audioStage1Path instanceof Error) return resolve(audioStage1Path);
+    if (audioStage1Path instanceof Error) return error(audioStage1Path);
+    filesToDelete.push(audioStage1Path);
 
     // generate vad data
     const audioStage1VadData = await this.python.runSilero(audioStage1Path);
-    if (audioStage1VadData instanceof Error) return resolve(audioStage1VadData);
+    if (audioStage1VadData instanceof Error) return error(audioStage1VadData);
+
+    // generate audio file
     let lastEnd = audioStage1VadData[audioStage1VadData.length - 1]?.end;
     if (audioStage1VadData.length === 1 && (lastEnd ?? 0) < 1000) {
       lastEnd = undefined;
     }
-
-    // generate audio file
     const audioStage2PathPromise = this.ffmpeg.process({
       inputPath: audioStage1Path,
       duration: lastEnd,
@@ -185,8 +201,8 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
       audioStage3PathPromise,
       imageDirPromise,
     ]);
-    if (audioStage3Path instanceof Error) return resolve(audioStage3Path);
-    if (imageDir instanceof Error) return resolve(imageDir);
+    if (audioStage3Path instanceof Error) return error(audioStage3Path);
+    if (imageDir instanceof Error) return error(imageDir);
 
     // TODO:
     //
@@ -236,19 +252,20 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
       audioStage2PathPromise,
       imagePathPromise,
     ]);
-    if (audioStage2Path instanceof Error) return resolve(audioStage2Path);
-    if (imagePath instanceof Error) return resolve(imagePath);
+    if (audioStage2Path instanceof Error) return error(audioStage2Path);
+    if (imagePath instanceof Error) return error(imagePath);
 
     const updateResult = await this.updateNote({
       note,
       picturePath: imagePath,
       sentenceAudioPath: audioStage2Path,
     });
-    if (updateResult instanceof Error) return resolve(updateResult);
+    if (updateResult instanceof Error) return error(updateResult);
 
     return resolve({
       sentenceAudioPath: audioStage2Path,
       picturePath: imagePath,
+      filesToDelete,
     });
   }
 
@@ -323,6 +340,14 @@ export class AnkiConnectClient extends ReconnectingAnkiConnect {
 
 function isNsfw(note: AnkiNote) {
   return note.tags.map((t) => t.toLowerCase()).includes("nsfw");
+}
+
+async function rm(filePath: string) {
+  try {
+    return await fs.rm(filePath);
+  } catch (e) {
+    return e instanceof Error ? e : new Error("Error when removing file");
+  }
 }
 
 export const zAnkiNote = z.object({
