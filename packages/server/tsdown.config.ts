46905a1bdecbe61d572f8dbde44f9cbfe40b1673
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-02-26 18:25:38 +0700

    feat: pnpm deploy

--- packages/server/tsdown.config.ts
+++ packages/server/tsdown.config.ts
@@ -2,19 +2,26 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/main.ts"],
+  noExternal: [/.*/, "@libsql/linux-x64-gnu"],
+  // nodeProtocol: true,
+  // unbundle: true,
   define: {
     __DEV__: "false",
   },
   copy: [
     "./drizzle/",
-    {
-      from: "../webui/dist",
-      to: "./dist",
-      rename: "webui",
-    },
+    { from: "../webui/dist", to: "./dist", rename: "webui" },
     {
       from: ["../python/pyproject.toml", "../python/uv.lock", "../python/src"],
       to: "./dist/python",
+    },
+    {
+      from: "node_modules/@libsql/linux-x64-gnu/index.node",
+      to: "./dist/node_modules/@libsql/linux-x64-gnu",
+    },
+    {
+      from: "node_modules/@libsql/win32-x64-msvc/index.node",
+      to: "./dist/node_modules/@libsql/win32-x64-msvc",
     },
   ],
 });
