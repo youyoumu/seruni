8197845939495ae8a19e54ced37708f439d96375
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-03-01 18:12:01 +0700

    feat: more format
    feat: add mp3

--- packages/server/src/exec/ffmpeg.exec.ts
+++ packages/server/src/exec/ffmpeg.exec.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 import type { State } from "#/state/state";
 import { yyyyMMdd_HHmmss } from "#/util/date";
 import { safeMkdir } from "#/util/fs";
@@ -6,6 +8,8 @@ import type { Logger } from "pino";
 import { uid } from "uid";
 
 import { Exec } from "./Exec";
+
+type Quality = "very-low" | "low" | "medium" | "high" | "very-high";
 
 interface SelectionData {
   x: number;
@@ -17,11 +21,54 @@ interface SelectionData {
 type ProcessFormat =
   | "wav"
   | "opus"
+  | "mp3"
   | "webp"
   | "webp:crop"
   | "webp:multiple"
-  | "webp:animated"
-  | "png:multiple";
+  | "jpeg"
+  | "jpeg:crop"
+  | "jpeg:multiple"
+  | "webp:animated";
+
+type ActualFormat = "wav" | "opus" | "mp3" | "webp" | "jpeg";
+
+function getPictureQuality(format: ActualFormat, quality: Quality = "medium"): string {
+  const jpegQualityMap: Record<Quality, string> = {
+    "very-low": "25",
+    low: "15",
+    medium: "8",
+    high: "4",
+    "very-high": "2",
+  };
+  const percentageQualityMap: Record<Quality, string> = {
+    "very-low": "35",
+    low: "50",
+    medium: "65",
+    high: "80",
+    "very-high": "95",
+  };
+  if (format === "jpeg") return jpegQualityMap[quality];
+  return percentageQualityMap[quality];
+}
+
+function getAudioQuality(format: ActualFormat, quality: Quality = "medium"): string {
+  const opusQualityMap: Record<Quality, string> = {
+    "very-low": "16k",
+    low: "32k",
+    medium: "64k",
+    high: "128k",
+    "very-high": "256k",
+  };
+  const mp3QualityMap: Record<Quality, string> = {
+    "very-low": "64k",
+    low: "128k",
+    medium: "192k",
+    high: "256k",
+    "very-high": "320k",
+  };
+  if (format === "opus") return opusQualityMap[quality];
+  return mp3QualityMap[quality];
+}
 
 export class FFmpegExec extends Exec {
   constructor(
@@ -56,42 +103,16 @@ export class FFmpegExec extends Exec {
     return R.succeed(result.value.stdout.split("\n")[0] ?? "");
   }
 
-  async process({
-    inputPath,
-    seek = 0,
-    duration,
-    format,
-    selectionData,
-  }: {
-    inputPath: string;
-    seek?: number;
-    duration?: number;
-    selectionData?: SelectionData;
-    format: ProcessFormat;
-  }): Promise<R.Result<string, Error>> {
-    const timestamp = this.createTimestamp();
-    const actualFormat = (() => {
-      if (format === "png:multiple") return "png";
-      if (format === "webp:crop") return "webp";
-      if (format === "webp:multiple") return "webp";
-      if (format === "webp:animated") return "webp";
-      return format;
-    })();
+  getActualFormat(format: ProcessFormat): ActualFormat {
+    if (format === "webp:crop") return "webp";
+    if (format === "webp:multiple") return "webp";
+    if (format === "webp:animated") return "webp";
+    if (format === "jpeg:crop") return "jpeg";
+    if (format === "jpeg:multiple") return "jpeg";
+    return format;
+  }
 
-    const tempPath = this.state.path().tempDir;
-    const outputPath = `${tempPath}/${timestamp}.${actualFormat}`;
-    const outputDir = `${tempPath}/${timestamp}`;
-    if (format.endsWith("multiple")) {
-      await safeMkdir(outputDir, { recursive: true });
-    }
-    const outputPattern = `${outputDir}/${timestamp}_%03d.${actualFormat}`;
-
-    const maxResolution = this.state.config().ffmpegMaxPictureResolution;
-    const scaleFilter = `scale='if(gt(iw,ih),-1,${maxResolution}):if(gt(ih,iw),-1,${maxResolution})':force_original_aspect_ratio=decrease`;
-
-    const defaultDuration = 1000;
-    const numberOfFrames = this.state.config().ffmpegPictureFrameCount;
-    const fps = numberOfFrames / ((duration ?? defaultDuration) / 1000);
+  getSelectionData(selectionData: SelectionData | undefined): SelectionData {
     let { x, y, width, height } = selectionData ?? {
       x: 0,
       y: 0,
@@ -102,128 +123,165 @@ export class FFmpegExec extends Exec {
     y = Math.floor(y);
     width = Math.floor(width);
     height = Math.floor(height);
-    seek = Math.floor(seek);
-    duration = duration === undefined ? undefined : Math.floor(duration);
+
+    return { x, y, width, height };
+  }
+
+  async process(opts: {
+    inputPath: string;
+    format: ProcessFormat;
+    seek?: number;
+    duration?: number;
+    selectionData?: SelectionData;
+  }): Promise<R.Result<string, Error>> {
+    const inputPath = opts.inputPath;
+    const format = opts.format;
+    const seek = Math.floor(opts.seek ?? 0);
+    const duration = opts.duration === undefined ? undefined : Math.floor(opts.duration);
+    const selectionData = opts.selectionData;
+
+    const timestamp = this.createTimestamp();
+    const actualFormat = this.getActualFormat(format);
+
+    const tempPath = this.state.path().tempDir;
+    const outputPath = `${path.join(tempPath, timestamp)}.${actualFormat}`;
+    const outputDir = path.join(tempPath, timestamp);
+    const outputPattern = path.join(outputDir, `${timestamp}_%03d.${actualFormat}`);
+    if (format.endsWith("multiple")) {
+      const result = await safeMkdir(outputDir, { recursive: true });
+      if (R.isFailure(result)) return R.fail(result.error);
+    }
+
+    const maxResolution = this.state.config().ffmpegMaxPictureResolution;
+    const scaleFilter = `scale='if(gt(iw,ih),-1,${maxResolution}):if(gt(ih,iw),-1,${maxResolution})':force_original_aspect_ratio=decrease`;
+
+    const requiredDuration = duration ?? 1000;
+    const numberOfFrames = this.state.config().ffmpegPictureFrameCount;
+    const fps = numberOfFrames / (requiredDuration / 1000);
+
+    const audioQuality = getAudioQuality(actualFormat);
+    const pictureQuality = getPictureQuality(actualFormat);
+
+    const audioParam = (format: "wav" | "opus" | "mp3") => {
+      const codecMap = {
+        wav: "pcm_s16le",
+        opus: "libopus",
+        mp3: "libmp3lame",
+      };
+      const samplingRateMap = {
+        wav: "44100",
+        opus: "48000",
+        mp3: "44100",
+      };
+
+      const seekParam = ["-ss", `${seek}ms`];
+      const inputParam = ["-i", inputPath];
+      const durationParam = duration !== undefined ? ["-t", `${duration}ms`] : [];
+      const stereoParam = ["-ac", "2"];
+      const samplingRateParam = ["-ar", samplingRateMap[format]];
+      const codecParam = ["-c:a", codecMap[format]];
+      const bitrateParam = format === "wav" ? [] : ["-b:a", audioQuality];
+
+      return [
+        "-y", // overwrite existing
+        ...seekParam,
+        ...inputParam,
+        ...durationParam,
+        ...stereoParam,
+        ...samplingRateParam,
+        ...codecParam,
+        ...bitrateParam,
+        "-vn", // no video
+        outputPath,
+      ];
+    };
+
+    const pictureParam = () => {
+      const seekParam = ["-ss", `${seek}ms`];
+      const inputParam = ["-i", inputPath];
+      const framesParam = ["-frames:v", "1"];
+      const filterParam = ["-vf", scaleFilter];
+      const qualityParam = ["-q:v", pictureQuality];
+
+      return [
+        "-y", // overwrite existing
+        ...seekParam,
+        ...inputParam,
+        ...framesParam,
+        ...filterParam,
+        ...qualityParam,
+        "-an", // no audio
+        outputPath,
+      ];
+    };
+
+    const pictureCropParam = () => {
+      const { x, y, width, height } = this.getSelectionData(selectionData);
+      const seekParam = ["-ss", `${seek}ms`];
+      const inputParam = ["-i", inputPath];
+      const framesParam = ["-frames:v", "1"];
+      const filterParam = ["-vf", `crop=${width}:${height}:${x}:${y}`];
+      const qualityParam = ["-q:v", pictureQuality];
+
+      return [
+        "-y", // overwrite existing
+        ...seekParam,
+        ...inputParam,
+        ...framesParam,
+        ...filterParam,
+        ...qualityParam,
+        "-an", // no audio
+        outputPath,
+      ];
+    };
+
+    const pictureMultipleParam = () => {
+      const seekParam = ["-ss", `${seek}ms`];
+      const inputParam = ["-i", inputPath];
+      const durationParam = ["-t", `${requiredDuration}ms`];
+      const frameRateParam = ["-r", `${fps}`];
+      const filterParam = ["-vf", scaleFilter];
+      const qualityParam = ["-q:v", pictureQuality];
+
+      return [
+        "-y", // overwrite existing
+        ...seekParam,
+        ...inputParam,
+        ...durationParam,
+        ...frameRateParam,
+        ...filterParam,
+        ...qualityParam,
+        "-an", // no audio
+        outputPattern,
+      ];
+    };
 
     const params: Record<ProcessFormat, string[]> = {
-      wav: [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        ...(duration !== undefined ? ["-t", `${duration}ms`] : []),
-        "-vn", // no video
-        "-acodec",
-        "pcm_s16le", // WAV codec
-        "-ar",
-        "44100", // sample rate
-        "-ac",
-        "2", // stereo
-        outputPath,
-      ],
-
-      opus: [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        ...(duration !== undefined ? ["-t", `${duration}ms`] : []),
-        "-vn", // no video
-        "-ac",
-        "2", // 1 = mono, 2 = stereo
-        "-ar",
-        "48000", // Opus requires 48kHz input
-        "-c:a",
-        "libopus", // use the Opus codec
-        "-b:a",
-        "64k", // bitrate (32 kbps is great for speech)
-        outputPath,
-      ],
-
-      webp: [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        "-frames:v",
-        "1", // only 1 frame
-        "-vf",
-        scaleFilter, // max resolution
-        "-q:v",
-        "75", // quality (1-100, worst to best)
-        outputPath,
-      ],
-
-      "webp:crop": [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        "-frames:v",
-        "1", // only 1 frame
-        "-vf",
-        `crop=${width}:${height}:${x}:${y}`, // crop to selection
-        "-q:v",
-        "75", // quality (1-100, worst to best)
-        outputPath,
-      ],
-
-      "webp:multiple": [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        "-t",
-        `${duration ?? defaultDuration}ms`, // duration
-        "-r",
-        `${fps}`, // frame rate
-        "-c:v",
-        "libwebp", // WebP codec
-        "-vf",
-        scaleFilter, // max resolution
-        "-q:v",
-        "75", // quality (1-100, worst to best)
-        outputPattern,
-      ],
-
-      "png:multiple": [
-        "-y", // overwrite existing
-        "-ss",
-        `${seek}ms`,
-        "-i",
-        inputPath, // input file
-        "-t",
-        `${duration ?? defaultDuration}ms`, // duration
-        "-r",
-        `${fps}`, // frame rate
-        "-vf",
-        scaleFilter, // max resolution
-        "-q:v",
-        "75", // quality (1-100, worst to best)
-        outputPattern,
-      ],
-
+      wav: audioParam("wav"),
+      opus: audioParam("opus"),
+      mp3: audioParam("mp3"),
+      webp: pictureParam(),
+      "webp:crop": pictureCropParam(),
+      "webp:multiple": pictureMultipleParam(),
+      jpeg: pictureParam(),
+      "jpeg:crop": pictureCropParam(),
+      "jpeg:multiple": pictureMultipleParam(),
       "webp:animated": [
         "-y", // overwrite existing
         "-ss",
         `${seek}ms`,
         "-i",
-        inputPath, // input file
+        inputPath,
         "-t",
-        `${duration ?? defaultDuration}ms`, // duration
+        `${requiredDuration}ms`,
         "-r",
-        "24", // frame rate
+        "24",
         "-c:v",
-        "libwebp_anim", // animated WebP codec
+        "libwebp_anim",
         "-vf",
-        scaleFilter, // max resolution
+        scaleFilter,
         "-q:v",
-        "75", // quality (1-100, worst to best)
+        pictureQuality,
         outputPath,
       ],
     };
