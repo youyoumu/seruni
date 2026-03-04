fd17a51394da5065de91529bb18d2699b6ffe7c5
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-03-04 12:38:57 +0700

    feat: disable webfontDownload

--- packages/webui/vite.config.ts
+++ packages/webui/vite.config.ts
@@ -5,7 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { bundleStats } from "rollup-plugin-bundle-stats";
 import { defineConfig } from "vite";
-import webfontDownload from "vite-plugin-webfont-dl";
+// import webfontDownload from "vite-plugin-webfont-dl";
 
 const config = defineConfig({
   resolve: {
@@ -21,11 +21,11 @@ const config = defineConfig({
     tanstackRouter({
       target: "react",
     }),
-    webfontDownload([
-      "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap",
-      "https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200..900&display=swap",
-      "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
-    ]),
+    // webfontDownload([
+    //   "https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap",
+    //   "https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200..900&display=swap",
+    //   "https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap",
+    // ]),
     bundleStats({
       outDir: "..",
     }),
